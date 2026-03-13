0271052880465eb042b25303ccd72261234cf07a
jinkai <leongjinkai@gmail.com>
2026-03-13 10:26:03 +0800

    shift pet listing page to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,44 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { fetchPets } from "@/lib/api";
+import { parseFiltersFromSearchParams } from "@/lib/filters";
+import { Pet, PetSpecies, PetSize } from "@/types/listing-types";
+import Image from "next/image";
+import { PetFilters } from "@/components/pet-filters";
 
-export default function Home() {
+interface ListingPageProps {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}
+
+export default async function ListingPage({ searchParams }: ListingPageProps) {
+  const resolvedSearchParams = await searchParams;
+  const filters = parseFiltersFromSearchParams(resolvedSearchParams);
+
+  const pets = await fetchPets(
+    Object.keys(filters).length > 0 ? filters : undefined
+  );
+
+  const uniqueSpecies = Object.values(PetSpecies);
+  const uniqueSizes = Object.values(PetSize);
+
+  const petListings = pets.map((pet: Pet): React.ReactNode => {
+    return (
+      <div key={pet.id}>
+        <h2>{pet.name}</h2>
+        <p>{pet.species}</p>
+        <p>{pet.price}</p>
+        <p>{pet.age_months}</p>
+        <p>{pet.size}</p>
+        <p>{pet.available ? "Available" : "Not Available"}</p>
+        <Image src={pet.image_url} alt={pet.name} width={100} height={100} />
+      </div>
+    );
+  });
   return (
     <div>
-      <h1>Home</h1>
-      <Button asChild>
-        <Link href="/listing">Pet Listings</Link>
-      </Button>
-      <Button asChild>
-        <Link href="/inquiry">Pet Inquiry</Link>
-      </Button>
+      <h1>Pet Listings</h1>
+      <div className="mb-4">
+        <PetFilters uniqueSpecies={uniqueSpecies} uniqueSizes={uniqueSizes} />
+      </div>
+      {petListings}
     </div>
   );
 }
